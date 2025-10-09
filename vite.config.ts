c6bb5cd15ad10944802300fe7e3f8022c112a23a
coder371 <abdalstar371371@gmail.com>
2025-10-09 15:14:29 +0300

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,43 @@
+import { reactRouter } from '@react-router/dev/vite';
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+const host = new URL(process.env.QUMRA_APP_URL || 'http://localhost').hostname;
+const port = Number(process.env.PORT || 3000);
+let hmrConfig;
+
+if (host === 'localhost') {
+  hmrConfig = {
+    protocol: 'ws',
+    host: 'localhost',
+    port: 64999,
+    clientPort: 64999,
+  };
+} else {
+  hmrConfig = {
+    protocol: 'wss',
+    host: host,
+    port: parseInt(process.env.FRONTEND_PORT!) || 8002,
+    clientPort: 443,
+  };
+}
+
+export default defineConfig({
+  server: {
+    allowedHosts: [host],
+    port,
+    cors: {
+      preflightContinue: true,
+    },
+    hmr: hmrConfig,
+    fs: {
+      // See https://vitejs.dev/config/server-options.html#server-fs-allow for more information
+      allow: ['app', 'node_modules'],
+    },
+  },
+  build: {
+    assetsInlineLimit: 0,
+  },
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+});
