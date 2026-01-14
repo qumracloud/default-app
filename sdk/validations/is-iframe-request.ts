edffbaa4e5a9104804110fd23d3eb6d421b7e977
mohamedmotea <devmote3@gmail.com>
2026-01-14 17:59:17 +0200

    Add embedded parameter check in isIframeRequest validation
    - Introduced a check for the 'embedded' query parameter in the isIframeRequest function to determine if the request should be treated as an iframe request.
    - Updated the function to return true if the 'embedded' parameter is set to "1".

--- sdk/validations/is-iframe-request.ts
+++ sdk/validations/is-iframe-request.ts
@@ -1,7 +1,12 @@
 export function isIframeRequest(request: Request): boolean {
+  const url = new URL(request.url);
+
   const secFetchDest = request.headers.get("sec-fetch-dest");
   const secFetchMode = request.headers.get("sec-fetch-mode");
   const secFetchSite = request.headers.get("sec-fetch-site");
+  const embedded  =  url.searchParams.get("embedded") === "1";
+
+  if (embedded) return true;
 
   // ✅ Browsers حديثة
   if (secFetchDest === "iframe") {
