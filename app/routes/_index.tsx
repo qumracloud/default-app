82b52e4e5752c67ba951e906b1cfdb2edf79b5e7
mohamedmotea <devmote3@gmail.com>
2026-01-15 09:02:04 +0200

    feat: add basic index route component

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,31 +1,4 @@
-import { prisma } from 'prisma/lib/prisma'
 import React from 'react'
-import { authenticate } from '~/qumra.server'
-
-
-
-export async function loader({request}:{request:Request}) {
-    const data = await prisma.session.findMany()
-    await authenticate.admin(request)
-  
-    // await prisma.session.create({data:{
-    //   store:"test",
-    //   accessToken:"test",
-    //   isOnline:true,
-    //   userId:"test",
-    //   firstName:"test",
-    //   lastName:"test",
-    //   email:"test",
-    //   emailVerified:true,
-    //   refreshToken:"test",
-    //   createdAt:new Date(),
-    //   lastSeenAt:new Date(),
-    // }})
-    return Response.json(data)
-    // console.log("🚀 ~ loader ~ admin:", admin.graphql(``,{}))
-    
-}
-
 
 function Index() {
   return (
