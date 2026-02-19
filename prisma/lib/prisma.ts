c185a821f962d786adc6867b6dfc3333dd9e355b
coder371 <abdalstar371371@gmail.com>
2026-02-19 22:30:45 +0200

    feat: Add `AppDistribution.AppStore` to `qumraApp` configuration and standardize Prisma client initialization.

--- prisma/lib/prisma.ts
+++ prisma/lib/prisma.ts
@@ -1,7 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 
 declare global {
-<<<<<<< HEAD
     // eslint-disable-next-line no-var
     var prismaGlobal: PrismaClient;
 }
@@ -10,16 +9,6 @@ if (process.env.NODE_ENV !== "production") {
     if (!global.prismaGlobal) {
         global.prismaGlobal = new PrismaClient();
     }
-=======
-  // eslint-disable-next-line no-var
-  var prismaGlobal: PrismaClient;
-}
-
-if (process.env.NODE_ENV !== "production") {
-  if (!global.prismaGlobal) {
-    global.prismaGlobal = new PrismaClient();
-  }
->>>>>>> 991b0ebe0017e5e18b52984a75ac679529f9e9eb
 }
 
 const prisma = global.prismaGlobal ?? new PrismaClient();
